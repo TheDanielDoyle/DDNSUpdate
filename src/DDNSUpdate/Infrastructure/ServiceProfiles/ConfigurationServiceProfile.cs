c77577e174a4848f3b8ca7558bd7d0834ec291eb
Daniel P. Doyle <3020488+TheDanielDoyle@users.noreply.github.com>
2020-09-16 20:59:15 +0100

    Added DDNSUpdateHostedService to run on an interval, which is specified in configuration.

---
+++ src/DDNSUpdate/Infrastructure/ServiceProfiles/ConfigurationServiceProfile.cs
@@ -0,0 +1,14 @@
+﻿using DDNSUpdate.Infrastructure.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using ServiceProfiles;
+
+namespace DDNSUpdate.Infrastructure.ServiceProfiles
+{
+    public class ConfigurationServiceProfile : HostedServiceProfile
+    {
+        public override void Configure(IHostServiceProfileContext context)
+        {
+            context.Services.Configure<ApplicationConfiguration>(context.Configuration);
+        }
+    }
+}
---
+++ src/DDNSUpdate/Infrastructure/ServiceProfiles/HostedServiceProfile.cs
@@ -0,0 +1,14 @@
+﻿using DDNSUpdate.Application;
+using Microsoft.Extensions.DependencyInjection;
+using ServiceProfiles;
+
+namespace DDNSUpdate.Infrastructure.ServiceProfiles
+{
+    public class HostedServiceProfile : HostServiceProfile
+    {
+        public override void Configure(IHostServiceProfileContext context)
+        {
+            context.Services.AddHostedService<DDNSUpdateHostedService>();
+        }
+    }
+}
