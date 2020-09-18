d204014fcf2a1bb3de139433e30e35c5973f7958
Michael Willis <71077044+mickey-willis@users.noreply.github.com>
2020-09-18 21:12:41 +0100

    Added External Address Providers to application configuration, and add unit test

--- src/DDNSUpdate.Tests/ApplicationConfigurationValidationTests.cs
+++ src/DDNSUpdate.Tests/ApplicationConfigurationValidationTests.cs
@@ -13,6 +13,21 @@ namespace DDNSUpdate.Tests
     public class ApplicationConfigurationValidationTests
     {
         [Theory]
+        [ClassData(typeof(ApplicationConfigurationInvalidExternalAddressProviders))]
+        public void ExternalAddressProvidersInvalid(IList<Uri> externalAddressProviders)
+        {
+            IValidator<ApplicationConfiguration> validator = new ApplicationConfigurationValidator();
+            ApplicationConfiguration configuration = CreateValidApplicationConfiguration();
+
+            configuration.ExternalAddressProviders = externalAddressProviders;
+
+            ValidationResult result = validator.Validate(configuration);
+
+            Assert.False(result.IsValid);
+            Assert.True(result.Errors.All(m => m.ErrorMessage.Equals(ApplicationConfigurationValidator.ExternalAddressProvidersErrorMessage)));
+        }
+
+        [Theory]
         [ClassData(typeof(ApplicationConfigurationUpdateIntervalInvalidTimeSpans))]
         public void UpdateIntervalInvalid(TimeSpan updateInterval)
         {
@@ -43,8 +58,23 @@ namespace DDNSUpdate.Tests
         {
             return new ApplicationConfiguration()
             {
+                ExternalAddressProviders = new List<Uri>() { new Uri("https://test.com/") },
                 UpdateInterval = ApplicationConfiguration.MinimumUpdateInterval
             };
+        }
+
+        private class ApplicationConfigurationInvalidExternalAddressProviders : IEnumerable<object[]>
+        {
+            public IEnumerator<object[]> GetEnumerator()
+            {
+                yield return new object[] { null };
+                yield return new object[] { new List<Uri>() };
+            }
+
+            IEnumerator IEnumerable.GetEnumerator()
+            {
+                return GetEnumerator();
+            }
         }
 
         private class ApplicationConfigurationUpdateIntervalInvalidTimeSpans : IEnumerable<object[]>
--- src/DDNSUpdate/Application/ApplicationConfigurationValidator.cs
+++ src/DDNSUpdate/Application/ApplicationConfigurationValidator.cs
@@ -5,10 +5,15 @@ namespace DDNSUpdate.Application
 {
     public class ApplicationConfigurationValidator : AbstractValidator<ApplicationConfiguration>
     {
+        public static readonly string ExternalAddressProvidersErrorMessage = $"{nameof(ApplicationConfiguration.ExternalAddressProviders)} cannot be empty.";
         public static readonly string UpdateIntervalErrorMessage = $"{nameof(ApplicationConfiguration.UpdateInterval)} must be greater than {ApplicationConfiguration.MinimumUpdateInterval:mm} minute/s.";
 
         public ApplicationConfigurationValidator()
         {
+            RuleFor(p => p.ExternalAddressProviders)
+                .NotEmpty()
+                .WithMessage(ExternalAddressProvidersErrorMessage);
+
             RuleFor(p => p.UpdateInterval)
                 .GreaterThanOrEqualTo(ApplicationConfiguration.MinimumUpdateInterval)
                 .WithMessage(UpdateIntervalErrorMessage);
