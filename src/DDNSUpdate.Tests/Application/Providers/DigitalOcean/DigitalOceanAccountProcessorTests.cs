3d758849d5e4143490ed5fe27f1966ac41f5e2ac
parkeradam <30392071+parkeradam@users.noreply.github.com>
2020-09-25 21:18:55 +0100

    Added AccountProcessorTests

--- src/DDNSUpdate.Tests/Application/Providers/DigitalOcean/DigitalOceanAccountProcessorTests.cs
+++ src/DDNSUpdate.Tests/Application/Providers/DigitalOcean/DigitalOceanAccountProcessorTests.cs
@@ -1,6 +1,51 @@
-﻿namespace DDNSUpdate.Tests.Application.Providers.DigitalOcean
+﻿using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using DDNSUpdate;
+using DDNSUpdate.Application.Providers.DigitalOcean;
+using DDNSUpdate.Application.Providers.DigitalOcean.Domain;
+using FakeItEasy;
+using FluentResults;
+using Xunit;
+
+namespace DDNSUpdate.Tests.Application.Providers.DigitalOcean
 {
     public class DigitalOceanAccountProcessorTests
     {
+        [Fact]
+        public async Task ReturnsSuccessfulResultWhenAllSucceed()
+        {
+            IDigitalOceanDomainProcessor domainProcessor = A.Fake<IDigitalOceanDomainProcessor>();
+            A.CallTo(() => domainProcessor.ProcessAsync(A<DigitalOceanDomain>.Ignored, A<Domain.ExternalAddress>.Ignored, A<string>.Ignored, A<CancellationToken>.Ignored)).Returns(Result.Ok());
+            DigitalOceanAccountProcessor processor = new DigitalOceanAccountProcessor(domainProcessor);
+
+            var account = new DigitalOceanAccount() { Domains = new List<DigitalOceanDomain>() { new DigitalOceanDomain(), new DigitalOceanDomain() } };
+            var externalAddress = new Domain.ExternalAddress() { IPv4Address = IPAddress.Parse("100.100.100.100") };
+
+            var result = await processor.ProcessAsync(account, externalAddress, new CancellationToken());
+
+            Assert.True(result.IsSuccess);
+        }
+
+        [Fact]
+        public async Task ReturnsFailureWhenAnyFail()
+        {
+            var domainOne = new DigitalOceanDomain();
+            var domainTwo = new DigitalOceanDomain();
+
+            IDigitalOceanDomainProcessor domainProcessor = A.Fake<IDigitalOceanDomainProcessor>();
+            A.CallTo(() => domainProcessor.ProcessAsync(domainOne, A<Domain.ExternalAddress>.Ignored, A<string>.Ignored, A<CancellationToken>.Ignored)).Returns(Result.Ok());
+            A.CallTo(() => domainProcessor.ProcessAsync(domainTwo, A<Domain.ExternalAddress>.Ignored, A<string>.Ignored, A<CancellationToken>.Ignored)).Returns(Result.Fail("This failed"));
+            DigitalOceanAccountProcessor processor = new DigitalOceanAccountProcessor(domainProcessor);
+
+            var account = new DigitalOceanAccount() { Domains = new List<DigitalOceanDomain>() { domainOne, domainTwo } };
+            var externalAddress = new Domain.ExternalAddress() { IPv4Address = IPAddress.Parse("100.100.100.100") };
+
+            var result = await processor.ProcessAsync(account, externalAddress, new CancellationToken());
+
+            Assert.True(result.IsFailed);
+        }
     }
 }
