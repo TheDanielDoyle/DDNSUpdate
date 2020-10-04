7af5d607edd0af8536b602263abb0acd190aed12
Daniel P. Doyle <3020488+TheDanielDoyle@users.noreply.github.com>
2020-10-04 23:29:38 +0100

    Another case of naming success message variable naming consistent.

--- src/DDNSUpdate/Application/Configuration/ConfigurationValidator.cs
+++ src/DDNSUpdate/Application/Configuration/ConfigurationValidator.cs
@@ -16,7 +16,7 @@ namespace DDNSUpdate.Application.Configuration
     public class ConfigurationValidator : IConfigurationValidator
     {
         private readonly ServiceFactory _serviceFactory;
-        private readonly string SuccessMessage = "Configuration Validated Successfully.";
+        private readonly string _successMessage = "Configuration Validated Successfully.";
 
         public ConfigurationValidator(ServiceFactory serviceFactory)
         {
@@ -33,7 +33,7 @@ namespace DDNSUpdate.Application.Configuration
             ValidationResultCollection results = new ValidationResultCollection(validationResults);
             if (results.IsValid)
             {
-                return Result.Ok().WithSuccess(SuccessMessage);
+                return Result.Ok().WithSuccess(_successMessage);
             }
             return results.ToResults();
         }
