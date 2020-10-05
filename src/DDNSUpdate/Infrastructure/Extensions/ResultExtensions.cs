f848d93c1b6b56a14d8796a6d10fe2f71359c556
Daniel P. Doyle <3020488+TheDanielDoyle@users.noreply.github.com>
2020-10-05 13:51:30 +0100

    Added ResultsLogger to remove logging dependant code from the hosted service. Tweaked the ResultExtension merge method to merge ther results in the correct order.

--- src/DDNSUpdate/Infrastructure/Extensions/ResultExtensions.cs
+++ src/DDNSUpdate/Infrastructure/Extensions/ResultExtensions.cs
@@ -16,15 +16,7 @@ namespace DDNSUpdate.Infrastructure.Extensions
 
         public static Result<T> Merge<T>(this Result<T> result, params Result[] results)
         {
-            foreach (Error error in results.SelectMany(r => r.Errors))
-            {
-                result = result.WithError(error);
-            }
-            foreach (Success success in results.SelectMany(r => r.Successes))
-            {
-                result = result.WithSuccess(success);
-            }
-            return result;
+            return result.WithReasons(results.SelectMany(r => r.Reasons));
         }
     }
 }
